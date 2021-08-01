dadd8c3ec4a65c9c7512218e736010fc203f8dfd
Pacifique Clement Ndayisenga <pacifiqueclement@gmail.com>
2021-08-01 18:33:15 +0200

    feature: add custom user model

---
+++ apps/users/model_managers.py
@@ -0,0 +1,55 @@
+from django.contrib.auth.base_user import BaseUserManager
+from django.utils.translation import gettext_lazy as _
+from django.core.validators import validate_email
+from django.core.exceptions import ValidationError
+
+
+class CustomerUserManager(BaseUserManager):
+    """Custom User Model Manager"""
+
+    def email_validator(self, email):
+        try:
+            validate_email(email)
+        except ValidationError:
+            raise ValueError(_("You must provide a valid email address"))
+
+    def create_user(self, email, password, **extra_fields):
+        """Create and save a user with email and password"""
+
+        if email:
+            email = self.normalize_email(email)
+            self.email_validator(email)
+        else:
+            raise ValidationError(_("User Account: An email address is required"))
+
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        """Create and save a superuser with email and password"""
+
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError(_("Superusers must have is_staff=True"))
+
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError(_("Superusers must have is_superuser=True"))
+
+        if email:
+            email = self.normalize_email(email)
+            self.email_validator(email)
+        else:
+            raise ValidationError(_("Superuser Account: An email address is required"))
+
+        user = self.create_user(email, password, **extra_fields)
+        user.save(using=self._db)
+
+        return user
--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from .model_managers import CustomerUserManager
+
+
+class User(AbstractUser):
+
+    username = None
+    email = models.EmailField(_("email address"), unique=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    object = CustomerUserManager()
+
+    def __str__(self):
+        return f"{self.first_name} - {self.last_name}"
+
+    def get_full_name(self):
+        full_name = f"{self.first_name} {self.last_name}"
+        return full_name.strip().title()
